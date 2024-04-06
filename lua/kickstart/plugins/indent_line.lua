64159de02217c13617b7297e32d531dbe7f095be
user-slinky <142591945+user-slinky@users.noreply.github.com>
2024-04-06 16:20:59 +0100

    Update indent_line.lua

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -2,8 +2,9 @@ return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      scope = { enabled = false },
+    },
   },
 }
