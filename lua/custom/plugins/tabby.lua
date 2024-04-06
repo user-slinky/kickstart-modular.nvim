a91d4d357d4803f7fdc6f66416440624fec0d5ad
user-slinky <142591945+user-slinky@users.noreply.github.com>
2024-04-06 17:32:06 +0100

    Update tabby.lua

--- lua/custom/plugins/tabby.lua
+++ lua/custom/plugins/tabby.lua
@@ -3,6 +3,27 @@ return {
     'nanozuki/tabby.nvim',
     event = 'VimEnter',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function() end,
+    config = function()
+    require('custom.plugins.tabby.tabline').use_preset('active_wins_at_tail', {
+  nerdfont = true, -- whether use nerdfont
+  theme = {
+    fill = 'TabLineFill', -- tabline background
+    head = 'TabLine', -- head element highlight
+    current_tab = 'TabLineSel', -- current tab label highlight
+    tab = 'TabLine', -- other tab label highlight
+    win = 'TabLine', -- window highlight
+    tail = 'TabLine', -- tail element highlight
+  },
+  lualine_theme = 'kanagawa', -- lualine theme name
+  buf_name = {
+    mode = 'unique',
+  },
+  tab_name = {
+    name_fallback = function(tabid)
+      return ''
+    end,
+  },
+})
+    end,
   },
 }
