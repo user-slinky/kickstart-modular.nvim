4852047d5b0e4f9a23dd0b47827a3c141b3f06d5
user-slinky <142591945+user-slinky@users.noreply.github.com>
2024-04-06 17:35:07 +0100

    Update leap.lua

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,3 +1,18 @@
 return {
-  { 'ggandor/leap.nvim', opts = {}, }
+  { 'ggandor/leap.nvim', 
+    config = function() 
+    require('custom.plugins.leap').create_default_mappings()
+-- Define equivalence classes for brackets and quotes, in addition to
+-- the default whitespace group.
+require('custom.plugins.leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+
+-- Override some old defaults - use backspace instead of tab (see issue #165).
+require('custom.plugins.leap').opts.special_keys.prev_target = '<backspace>'
+require('custom.plugins.leap').opts.special_keys.prev_group = '<backspace>'
+
+-- Use the traversal keys to repeat the previous motion without explicitly
+-- invoking Leap.
+require('custom.plugins.leap.user').set_repeat_keys('<enter>', '<backspace>')
+    end
+    opts = {}, }
 }
