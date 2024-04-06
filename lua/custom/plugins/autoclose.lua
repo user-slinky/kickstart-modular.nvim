c1e344a253144fd33f26ed5aa4a426155da90617
user-slinky <142591945+user-slinky@users.noreply.github.com>
2024-04-06 16:57:37 +0100

    Update autoclose.lua

--- lua/custom/plugins/autoclose.lua
+++ lua/custom/plugins/autoclose.lua
@@ -1,1 +1,15 @@
-
+return = {
+    {
+    'm4xshen/autoclose.nvim',
+    opts = {
+      keys = {
+        ['<'] = {
+          escape = false,
+          close = true,
+          pair = '<>',
+          disabled_filetypes = {},
+        },
+      },
+    },
+  },
+}
