01eb5b7b1a177a1d1c1bd984998353e03da289f7
user-slinky <142591945+user-slinky@users.noreply.github.com>
2024-04-06 16:53:11 +0100

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,33 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+require('leap').create_default_mappings()
+-- Define equivalence classes for brackets and quotes, in addition to
+-- the default whitespace group.
+require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+
+-- Override some old defaults - use backspace instead of tab (see issue #165).
+require('leap').opts.special_keys.prev_target = '<backspace>'
+require('leap').opts.special_keys.prev_group = '<backspace>'
+
+-- Use the traversal keys to repeat the previous motion without explicitly
+-- invoking Leap.
+require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+
+require('tabby.tabline').use_preset('active_wins_at_tail', {
+  nerdfont = true, -- whether use nerdfont
+  theme = {
+    fill = 'TabLineFill', -- tabline background
+    head = 'TabLine', -- head element highlight
+    current_tab = 'TabLineSel', -- current tab label highlight
+    tab = 'TabLine', -- other tab label highlight
+    win = 'TabLine', -- window highlight
+    tail = 'TabLine', -- tail element highlight
+  },
+  lualine_theme = 'kanagawa', -- lualine theme name
+  buf_name = {
+    mode = 'unique',
+  },
+  tab_name = {
+    name_fallback = function(tabid)
+      return ''
+    end,
+  },
+})
