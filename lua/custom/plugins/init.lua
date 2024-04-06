9c369e8fb3a97e7138388a3e20ca4cf94164d3dc
user-slinky <142591945+user-slinky@users.noreply.github.com>
2024-04-06 17:29:27 +0100

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,17 +1,17 @@
-require('leap').create_default_mappings()
+require('custom.plugins.leap').create_default_mappings()
 -- Define equivalence classes for brackets and quotes, in addition to
 -- the default whitespace group.
-require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+require('custom.plugins.leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
 
 -- Override some old defaults - use backspace instead of tab (see issue #165).
-require('leap').opts.special_keys.prev_target = '<backspace>'
-require('leap').opts.special_keys.prev_group = '<backspace>'
+require('custom.plugins.leap').opts.special_keys.prev_target = '<backspace>'
+require('custom.plugins.leap').opts.special_keys.prev_group = '<backspace>'
 
 -- Use the traversal keys to repeat the previous motion without explicitly
 -- invoking Leap.
-require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+require('custom.plugins.leap.user').set_repeat_keys('<enter>', '<backspace>')
 
-require('tabby.tabline').use_preset('active_wins_at_tail', {
+require('custom.plugins.tabby.tabline').use_preset('active_wins_at_tail', {
   nerdfont = true, -- whether use nerdfont
   theme = {
     fill = 'TabLineFill', -- tabline background
